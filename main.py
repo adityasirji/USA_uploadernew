7673ddb8775083b5504a9a93a61ac08d13bd3c61
pjvip <140431425+pjvip@users.noreply.github.com>
2023-09-30 10:08:11 +0530

    Update main.py

--- main.py
+++ main.py
@@ -28,8 +28,8 @@ from dotenv import load_dotenv
 load_dotenv()
 os.makedirs("./downloads", exist_ok=True)
 
-AUTH_USERS = 6674083654
-sudo_users = [-1001689381678]
+AUTH_USERS = 1112773045
+sudo_users = [-1112773045]
 bot = Client(
     "bot",
              bot_token=os.environ.get("BOT_TOKEN"),
